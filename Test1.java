06a8666aa9e8958b575c1dd38bf3f33bb1e227ae
DmytroKuzin <dimabeton@mail.ru>
2016-05-19 15:53:53 +0300

    evrika

--- Test1.java
+++ Test1.java
@@ -35,6 +35,9 @@ public class Test1 {
     System.out.println("Hello Kostya!");
 
     System.out.println("Hello Taras!");
+            //Kto zhe protiv, glavnij vopros - kak zalit s brenchi master v brenchu druguju?
+            //Oj, kazhetsa ponal, shas sdelaju!
+            
 
 
 
