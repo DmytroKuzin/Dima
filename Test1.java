ad7da47b80385cd70ba482290b3bca305a9be24a
kostyantins <kostyantins@mail.ru>
2016-05-19 14:38:44 +0200

    sout 02

--- Test1.java
+++ Test1.java
@@ -3,4 +3,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello Dima!");
+
+        System.out.println("Hello Kostya!");
 }
