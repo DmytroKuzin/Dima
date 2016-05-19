550311f2aaa99272e8cafa702b491ecf2aa4f6c3
kostyantins <kostyantins@mail.ru>
2016-05-19 14:50:49 +0200

    sout 03

--- Test1.java
+++ Test1.java
@@ -2,7 +2,22 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello Dima!");
 
-        System.out.println("Hello Kostya!");
+    }
+
+    //konflict
+    //Privet
+    // Kak Dimon zaebalsa!!!
+    System.out.println("Hello Dima!");
+    // Chto proishodit?
+
+
+    // Davay dadim duplya!!
+
+
+    System.out.println("Hello Kostya!");
+
+    System.out.println("Hello Taras!");
+
+
 }
